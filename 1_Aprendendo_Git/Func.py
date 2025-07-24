0128b7c134a8ecda18a2b2fdc49c8c5d7849cd6c
João Vitor <joaovitorlemes85@gmail.com>
2025-07-24 11:40:07 -0300

    Comitando func

---
+++ 1_Aprendendo_Git/Func.py
@@ -0,0 +1,19 @@
+def parouimpar(number):
+    if number % 2 == 0:
+        print("O numero é par")
+    else:
+        print("O numero não é par")
+    
+parouimpar(25)
+
+
+
+def mult(*args):
+    total = 0
+    for i in args:
+        total += i
+        
+    return total
+
+print(mult(1,2,3,5,9))
+        
