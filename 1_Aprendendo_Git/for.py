81d7cd4c61719282a1f241cdef564b71d62e5dd9
João Vitor <joaovitorlemes85@gmail.com>
2025-07-28 16:04:45 -0300

    Commitando for

--- 1_Aprendendo_Git/for.py
+++ 1_Aprendendo_Git/for.py
@@ -1,18 +1,44 @@
-quadrados = [i**2 for i in range(5)]
-print(quadrados)
-print("######################################")
-for numero in sorted([3, 1, 2]):
-    print(numero)
+#quadrados = [i**2 for i in range(5)]
+#print(quadrados)
+#print("######################################")
 
-print("######################################")
-maiusc = [item.upper() for item in ["joao", "li", "maria", "leopoldo", "leoncio", "gertrudes"] if len(item) >4]
-print(maiusc)
-print("######################################")
+#for numero in sorted([3, 1, 2]):
+#    print(numero)
+
+print("Encontre todas as palavras em uma sequência que tenham menos de 4 letras")
+maiusc = [item.upper() for item in ["joao", "li", "maria", "leopoldo", "leoncio", "gertrudes"] if len(item) <=4]
+print(maiusc,"\n")
+
+
 div3 = [i for i in range(30) if i%3 ==0]
-print(div3)
+print(div3, "\n")
 
-print("######################################")
+print("Produza uma lista contendo a palavra par se um número na lista for par, e a palavra ímpar se o número for ímpar ")
 lista = [2,4,6,10, 15, 20, 25, 50, 63, 2587]
-
 nova = ["par" if i % 2 ==0 else "impar" for i in lista]
-print(nova)
+print(nova, "\n")
+
+
+print("Dada uma lista de números, remova todos os números ímpares da lista:")
+lista1 = [1,6,7,9,10,15,24,56]
+nova_lista1 = [number for number in lista1 if number %2 ==0]
+print(nova_lista1, "\n")
+
+
+print("Encontre todos os números de 1 a 1000 que são divisíveis por 7:")
+nova_lista2 = [number for number in range(100) if number %7 ==0]
+print(nova_lista2, "\n")
+
+
+print("Encontre todos os números de 1 a 1000 que tenham um 3 neles:")
+nova_lista3 = [number for number in range(100) if "3" in str(number) ]
+print(nova_lista3, "\n")
+
+print("Contar o número de espaços em uma string")
+novalista4 = len([space for space in "agsdfjad hdfgkfksadk afkadafa sadfdafdf" if " " in space])
+print(novalista4, "\n")
+
+print("Obtenha apenas os números em uma frase")
+frase = "Em 1984, houve 13 ocorrências de um protesto com mais de 1.000 pessoas presentes"
+novalista5 = [num for num in frase if num.isnumeric()]
+print(novalista5, "\n")
