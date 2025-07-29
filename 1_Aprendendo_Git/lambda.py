7a6e0d288c75d3d336dc2d8191a51e788ddf46ab
Joao Vitor <joaovitorlemes85@gmail.com>
2025-07-29 07:09:51 -0300

    aprendendo lambda

--- 1_Aprendendo_Git/lambda.py
+++ 1_Aprendendo_Git/lambda.py
@@ -9,3 +9,18 @@ print(const())
 #Par ou Ímpar
 e_par = lambda x: "par" if x%2 ==0 else "impar"
 print(e_par(5833))
+
+#Usando Map
+num = [2,4,6,8]
+quad = tuple(map(lambda x:x**2, num))
+print(quad)
+
+#Usando Filter
+lista = [1,5,8,6,97,458,124]
+pares = list(filter(lambda x:x%2==0, lista))
+print(pares)
+
+#Usando Sorted
+nomes = ['joao', 'godofredo', 'alice']
+ordenados = sorted(nomes, key = lambda nome: len(nome)) #Ordandando pelo tamanho da palavra
+print(ordenados)
